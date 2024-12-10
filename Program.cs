b3e57b90d2a799e6496bd378938434d099524fb2
rayray39 <raynerlimfy@gmail.com>
2024-12-10 13:53:11 +0800

    Add authorisation service to builder

--- Program.cs
+++ Program.cs
@@ -27,6 +27,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         }; 
     });
 
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 // Add authentication middleware 
